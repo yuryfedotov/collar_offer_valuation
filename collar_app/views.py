333e83b915ad47345ef0ca19948322c9ebe0093c
yuryfedotov <urij@MBP-Urij.home>
2020-04-10 20:26:35 +0200

    Changed my username

--- collar_app/views.py
+++ collar_app/views.py
@@ -7,5 +7,5 @@ def index(request):
 def add(request):
     val1 = int(request.GET["num1"])
     val2 = int(request.GET["num2"])
-    res = val1 + val2
+    res = val1 + val2 #
     return render(request, 'result.html', {'result': res})
