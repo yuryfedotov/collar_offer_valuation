be31c0a885b405dbd444e71d66c16c352d3d568d
yuryfedotov <urij@MBP-Urij.home>
2020-04-11 16:05:54 +0200

    Added 2x3 bokeh layout with data from the form, and centered it. Also configured static files.

--- collar_app/views.py
+++ collar_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from bokeh.plotting import figure
+from bokeh.layouts import gridplot
+from bokeh.embed import components
 
 def index(request):
     return render(request, 'index.html')
 
 def calculate_market_cap(request):
-    company_name = request.POST.get("bidderName")
+    
+    """ WORKED
+    company_name = str(request.POST.get("bidderName"))
     nshares = int(request.POST.get("bidderSharesBefore"))
     share_price = float(request.POST.get("bidderPriceBefore"))
     market_cap = nshares * share_price
@@ -12,4 +17,33 @@ def calculate_market_cap(request):
         'company_name': company_name,
         'market_cap': market_cap,
     }
+    """
+    
+    company_name = str(request.POST.get("bidderName"))
+    nshares = int(request.POST.get("bidderSharesBefore"))
+    share_price = float(request.POST.get("bidderPriceBefore"))
+    market_cap = nshares * share_price
+
+    chart1 = figure()
+    chart1.line([1,2], [nshares, share_price], line_width=2, line_color='blue')
+
+    chart2 = figure()
+    chart2.line([1,2], [nshares, share_price], line_width=2, line_color='red')
+
+    chart3 = figure()
+    chart3.line([1,2], [nshares, share_price], line_width=2, line_color='green')
+
+    chart4 = figure()
+    chart4.line([1,2], [nshares, share_price], line_width=2, line_color='purple')
+
+    grid = gridplot([[chart1, chart2, None], [chart3, None, chart4]], plot_width=300, plot_height=250)
+    script_grid, div_grid = components(grid)
+
+    context = {
+        'company_name': company_name,
+        'market_cap': market_cap,
+        'script_grid': script_grid,
+        'div_grid': div_grid
+    }
+
     return render(request, 'result.html', context)
--- collar_project/settings.py
+++ collar_project/settings.py
@@ -118,7 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'collar_project/static'),
+]
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
